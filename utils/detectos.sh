777b68538780a4be47eebfe396a328f8454c5120
Alexey Nikitin <alexeyn@cqg.com>
2018-07-23 12:54:03 +0400

    Update zshrc and dectectos.sh

--- utils/detectos.sh
+++ utils/detectos.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+if [ -z $OSTYPE ] ; then
+  OSTYPE="$(uname -o)"
+fi
+
 case "$OSTYPE" in
   solaris*) echo "SOLARIS" ;;
   darwin*)  echo "OSX" ;; 
   linux*)   echo "LINUX" ;;
+  Linux*)   OS=LINUX ;;
   bsd*)     echo "BSD" ;;
   *)        echo "unknown" ;;
 esac
