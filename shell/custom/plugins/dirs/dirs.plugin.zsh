d3dfad53106f6ace7e979481c6bb4f5520245e52
Alexey Nikitin <alexeyn@cqg.com>
2019-03-11 11:39:19 +0400

    Add alias for code . as c

--- shell/custom/plugins/dirs/dirs.plugin.zsh
+++ shell/custom/plugins/dirs/dirs.plugin.zsh
@@ -14,6 +14,8 @@ alias glsub="ls -1d */ | xargs -L1d -I{} -- sh -c \"pushd {} > /dev/null ; echo 
 alias gstsub="ls -1d */ | xargs -L1d -I{} -- sh -c \"pushd {} > /dev/null ; echo {}; git status -s ; popd > /dev/null\""
 alias gbrsub="ls -1d */ | xargs -L1d -I{} -- sh -c \"pushd {} > /dev/null ; echo {}; git status | head -n 1; echo \"\"; popd > /dev/null\""
 
+alias c="code ."
+
 goenv () {
     export GOPATH=${PWD}
     echo $PWD
